205d17ffd8efdca67c4ff6809044865645900b73
Liad Nachlai <liad.nachlai@gmail.com>
2024-11-19 16:02:28 +0200

    Slight design change in the category cards

--- app/src/main/java/com/example/recipeapp/ui/mainscreen/RecipeCategoryDisplay.kt
+++ app/src/main/java/com/example/recipeapp/ui/mainscreen/RecipeCategoryDisplay.kt
@@ -2,20 +2,22 @@ package com.example.recipeapp.ui.mainscreen
 
 
 import androidx.compose.animation.animateContentSize
+import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.grid.GridCells
-import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
-import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.foundation.lazy.staggeredgrid.LazyVerticalStaggeredGrid
+import androidx.compose.foundation.lazy.staggeredgrid.StaggeredGridCells
+import androidx.compose.foundation.lazy.staggeredgrid.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.KeyboardArrowDown
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -25,6 +27,8 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
@@ -39,12 +43,11 @@ fun CategoryGrid(
     modifier: Modifier = Modifier,
     contentPadding: PaddingValues = PaddingValues(4.dp),
 ) {
-    LazyVerticalGrid(
-        columns = GridCells.Adaptive(minSize = 128.dp),
+    LazyVerticalStaggeredGrid(
+        columns = StaggeredGridCells.Adaptive(minSize = 128.dp),
         modifier = modifier.padding(horizontal = 4.dp),
         contentPadding = contentPadding,
         horizontalArrangement = Arrangement.spacedBy(8.dp),
-        verticalArrangement = Arrangement.spacedBy(8.dp)
     ) {
         items(categoryList) { category ->
             CategoryCard(
@@ -60,6 +63,9 @@ fun CategoryGrid(
 @Composable
 fun CategoryCard(category: Category, modifier: Modifier = Modifier) {
     var expanded by remember { mutableStateOf(false) }
+    val rotationState by animateFloatAsState(
+        targetValue = if (expanded) 180f else 0f, label = ""
+    )
 
     Column {
         Card(
@@ -74,23 +80,32 @@ fun CategoryCard(category: Category, modifier: Modifier = Modifier) {
                 horizontalAlignment = Alignment.CenterHorizontally,
                 modifier = Modifier
                     .fillMaxWidth()
+
             ) {
                 Text(
                     category.name,
-                    style = MaterialTheme.typography.bodyLarge,
-                    modifier = Modifier.padding(4.dp),
+                    style = MaterialTheme.typography.titleMedium,
+                    modifier = Modifier.padding(8.dp),
                     textAlign = TextAlign.Center
                 )
                 if (expanded) {
                     SubCategoryList(
                         category.subCategoryList,
-                        modifier = Modifier.animateContentSize()
+                        modifier = Modifier
                     )
                 }
-                Icon(
-                    Icons.Filled.KeyboardArrowDown,
-                    contentDescription = "Show more",
-                )
+                IconButton(
+                    modifier = Modifier
+                        .alpha(0.2f)
+                        .rotate(rotationState),
+                    onClick = {
+                        expanded = !expanded
+                    }) {
+                    Icon(
+                        Icons.Filled.KeyboardArrowDown,
+                        contentDescription = "Show more",
+                    )
+                }
 
 
             }
@@ -102,7 +117,8 @@ fun CategoryCard(category: Category, modifier: Modifier = Modifier) {
 
 @Composable
 fun SubCategoryList(subCategoryList: List<SubCategory>, modifier: Modifier = Modifier) {
-    Column(modifier) {
+    Column(modifier,
+            ) {
         subCategoryList.forEach { subCategory ->
             Row {
                 Text(
