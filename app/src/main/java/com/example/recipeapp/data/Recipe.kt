413431f39d729a25af9394a225575a6ba3f5f9b3
Liad Nachlai <liad.nachlai@gmail.com>
2024-12-17 10:51:23 +0200

    Updated recipe data class

--- app/src/main/java/com/example/recipeapp/data/Recipe.kt
+++ app/src/main/java/com/example/recipeapp/data/Recipe.kt
@@ -5,12 +5,13 @@ import kotlinx.serialization.Serializable
 import kotlinx.serialization.json.Json
 import kotlin.time.Duration
 
-@Serializable
+
 
 /**
  * Data class for recipes in the recipe browsing screen.
  * Matches the format received from `/categories/{categoryId}/subcategories/{subcategoryId}/recipes`.
  */
+@Serializable
 data class PartialRecipe(
     val id: Int,
     val name: String,
@@ -23,12 +24,26 @@ data class PartialRecipe(
  * Each recipe is made up of recipe parts.
  * Matches the format received from `/recipes/{recipeId}`.
  */
+@Serializable
 data class Recipe(
     val id: Int,
     val name: String,
-    val cookTime: List<Duration>,
+    val description: String,
+    val cookTime: CookTime,
     val recipeParts: List<RecipePart>,
     val subcategoryId: Int
+)
+
+/**
+ * Cook time for the full recipe displayed in the selected recipe screen.
+ * `handsOn`  refers to the time require for the preparation of the recipe,
+ * while `readyIn` refers to the total time it takes until the dish is ready.
+ * Matches the format of `cookTime` received from `/recipes/{recipeId}`.
+ */
+@Serializable
+data class CookTime(
+    val handsOn: Duration,
+    val readyIn: Duration
 )
 
 /**
@@ -37,7 +52,7 @@ data class Recipe(
  * For example, a pie recipe would contain a crust recipe part, with its' matching ingredients and instructions.
  * Matches the format received from `/recipes/{recipeId}`.
  */
-
+@Serializable
 data class RecipePart(
     val name: String,
     val ingredients: List<String>,
