2cffffa683e54635baa029cb13becb534990c4c4
Liad Nachlai <liadnachlai@gmail.com>
2025-05-20 16:11:10 +0300

    Fixed check boxes resetting when switching tabs or to other screens

--- app/src/main/java/sh/deut/recipeapp/ui/RecipeUiState.kt
+++ app/src/main/java/sh/deut/recipeapp/ui/RecipeUiState.kt
@@ -1,8 +1,10 @@
 package sh.deut.recipeapp.ui
 
 import sh.deut.recipeapp.data.Category
+import sh.deut.recipeapp.data.CookTime
 import sh.deut.recipeapp.data.PartialRecipe
 import sh.deut.recipeapp.data.Recipe
+import sh.deut.recipeapp.data.RecipePart
 import sh.deut.recipeapp.data.SubCategory
 
 data class RecipeUiState(
@@ -15,10 +17,36 @@ data class RecipeUiState(
 
 // Selected recipe screen ui state
     val selectedRecipeName: String = "",
-    val selectedRecipe: Recipe? = null
+    val selectedRecipe: UiRecipe? = null
 )
 
 data class CategoryCardState(
     val category: Category,
     val isExpanded: Boolean = false,
+)
+
+data class UiRecipe(
+    val id: Int,
+    val name: String,
+    val imgUrl: String,
+    val description: String,
+    val cookTime: CookTime,
+    val recipeParts: List<UiRecipePart>,
+    val subcategoryId: Int
+)
+
+data class UiRecipePart(
+    val name: String,
+    val ingredients: List<UiIngredient>,
+    val instructions: List<UiInstruction>
+)
+
+data class UiIngredient(
+    val ingredient: String,
+    val isChecked: Boolean = false
+)
+
+data class UiInstruction(
+    val instruction: String,
+    val isChecked: Boolean = false
 )
