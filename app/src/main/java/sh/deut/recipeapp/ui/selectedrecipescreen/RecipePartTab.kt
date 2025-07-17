d7cf7df5b43ecccda433b2860f4520984c10800a
Liad Nachlai <liadnachlai@gmail.com>
2025-07-17 19:58:25 +0300

    Sticky header for tab row

--- app/src/main/java/sh/deut/recipeapp/ui/selectedrecipescreen/RecipePartTab.kt
+++ app/src/main/java/sh/deut/recipeapp/ui/selectedrecipescreen/RecipePartTab.kt
@@ -4,8 +4,12 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Checkbox
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
@@ -34,7 +38,8 @@ fun RecipePartTab(
     onIngredientCheckedChanged: (Boolean, Int) -> Unit
 ) {
     Column(
-        modifier = modifier.padding(dimensionResource(R.dimen.padding_small)),
+        modifier = modifier
+            .padding(dimensionResource(R.dimen.padding_small))
     ) {
         Text(
             modifier = Modifier.padding(bottom = dimensionResource(R.dimen.padding_small)),
